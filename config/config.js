48c3f6c5eae0e874f002fa306fe9483c8eee6936
Jesper Orb <github@jesperorb.com>
2021-10-24 10:05:59 +0200

    refactor: freshen up :nails:

--- config/config.js
+++ config/config.js
@@ -1,15 +1,26 @@
-let config = {
-    apiUrl: process.env.API_URL,
-    apiKeyName: process.env.API_KEY_NAME,
-    apiKeyValue: process.env.API_KEY_VALUE,
-    port: process.env.PORT || 3000,
-    assignKey: assignKey
-}
-
-function assignKey(query){
+const assignKey = (query) => {
   let obj = {};
   obj[config.apiKeyName] = config.apiKeyValue;
   return Object.assign(query, obj);
-}
+};
 
-module.exports = config;
+const logPort = (port) => {
+  return () =>
+    console.log(
+      `[\x1b[34mSERVER\x1b[37m] Listening on port: \x1b[36m${port} 🤖 \x1b[37m`
+    );
+};
+
+const config = {
+  apiUrl: process.env.API_URL,
+  apiKeyName: process.env.API_KEY_NAME,
+  apiKeyValue: process.env.API_KEY_VALUE,
+  port: process.env.PORT || 3000,
+  assignKey: assignKey,
+};
+
+module.exports = {
+  config,
+  assignKey,
+  logPort,
+};
